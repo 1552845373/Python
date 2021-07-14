7326f3c77b3a4acb3b887a544941b41d3fd8de60
1552845373 <1552845373@qq.com>
2021-07-14 16:40:32 +0800

    tset

--- pyqt5/first.py
+++ pyqt5/first.py
@@ -5,14 +5,34 @@
 # @File : first.py
 # @Software : PyCharm
 
+# import sys
+# import untitled
+# from PyQt5.QtWidgets import QApplication,QMainWindow
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     mainWindow = QMainWindow()
+#     ui = untitled.Ui_MainWindow()
+#     ui.setupUi(mainWindow)
+#     mainWindow.show()
+#     sys.exit(app.exec_())
+
 import sys
-import untitled
 from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtGui import QIcon
+
+class FirstMainWin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("第一个主窗口应用")
+        self.resize(400,300)
+        self.status = self.statusBar()
+        self.status.showMessage("只存在五秒",msecs=5000)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWindow = QMainWindow()
-    ui = untitled.Ui_MainWindow()
-    ui.setupUi(mainWindow)
+    mainWindow = FirstMainWin()
     mainWindow.show()
     sys.exit(app.exec_())
+
