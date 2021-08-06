48f5d5519bd8bcbec45b84f834a3d0d7ad8c594e
1552845373 <1552845373@qq.com>
2021-08-06 14:45:09 +0800

    test

--- pyqt5/first.py
+++ pyqt5/first.py
@@ -5,44 +5,44 @@
 # @File : first.py
 # @Software : PyCharm
 
-# import sys
-# import untitled
-# from PyQt5.QtWidgets import QApplication,QMainWindow
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     mainWindow = QMainWindow()
-#     ui = untitled.Ui_MainWindow()
-#     ui.setupUi(mainWindow)
-#     mainWindow.show()
-#     sys.exit(app.exec_())
-
 import sys
-from PyQt5.QtWidgets import QDesktopWidget,QApplication,QMainWindow
-from PyQt5.QtGui import QIcon
-
-class FirstMainWin(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("第一个主窗口应用")
-        self.resize(400,300)
-        self.status = self.statusBar()
-        self.status.showMessage("只存在五秒",msecs=5000)
-
-    def center(self):
-        # 获取屏幕坐标系
-        screen = QDesktopWidget().screenGeometry()
-        # 获取窗口坐标系
-        size = self.geometry()
-        newLeft = (screen.width() - size.width()) / 2
-        newTop = (screen.height() - size.height()) / 2
-        self.move(int(newLeft),int(newTop))
+import untitled
+from PyQt5.QtWidgets import QApplication,QMainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWindow = FirstMainWin()
-    mainWindow.center()
+    mainWindow = QMainWindow()
+    ui = untitled.Ui_MainWindow()
+    ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
+
+# import sys
+# from PyQt5.QtWidgets import QDesktopWidget,QApplication,QMainWindow
+# from PyQt5.QtGui import QIcon
+#
+# class FirstMainWin(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.setWindowTitle("第一个主窗口应用")
+#         self.resize(400,300)
+#         self.status = self.statusBar()
+#         self.status.showMessage("只存在五秒",msecs=5000)
+#
+#     def center(self):
+#         # 获取屏幕坐标系
+#         screen = QDesktopWidget().screenGeometry()
+#         # 获取窗口坐标系
+#         size = self.geometry()
+#         newLeft = (screen.width() - size.width()) / 2
+#         newTop = (screen.height() - size.height()) / 2
+#         self.move(int(newLeft),int(newTop))
+#
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     mainWindow = FirstMainWin()
+#     mainWindow.center()
+#     mainWindow.show()
+#     sys.exit(app.exec_())
 
